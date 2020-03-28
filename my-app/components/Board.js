d0ca1c3f8fb7426a7f113c0446320f92a9c24ca3
Gabriel <gabriel.i.rueda@gmail.com>
2020-03-28 11:15:52 -0400

    Win Detection is Complete

--- my-app/components/Board.js
+++ my-app/components/Board.js
@@ -8,75 +8,118 @@ export default function Board() {
         // 1  4  7 
         // 2  5  8
         // 3  6  9
-        box: ["", "", "", "", "", "", "", "", ""],
-        status: "no winner"
+        symbols: ["","O","X"],
+        box: [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+        ],
+        status: false,
+        name: "Gabriel",
     });
     const borderWidth = 3;
-    const ClickMe = (id) => {
-        if(state.box[(id-1)] == ""){
+    const ClickMe = (y,x) => {
+        if(state.box[y][x] == ""){
             let newState = Object.assign({}, state);
             if(state.turnCounter% 2 == 0){
-                newState.box[(id-1)] = "X";
+                newState.box[y][x] = 2;
             }else{
-                newState.box[(id-1)] = "O";
+                newState.box[y][x] = 1;
             }
-
             newState.turnCounter++;
+            newState.status = solver().toString();
             setState(newState);
-            checkWin("X");
         }
     };
-    //Decrease all the numbers inside box[...] by 1 when finished
-    const checkWin = (sym) => {
-        // if(state.box[5] == sym){
-            
-        // }
-        for(var i = 0; i<state.box.length; i++){
-            console.log(i)
-        }
-    }
+    
     const resetBoard = () => {
         let newState = Object.assign({}, state);
-        newState.box = ["", "", "", "", "", "", "", "", ""];
+        newState.box = [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+        ];
+        newState.status = false
+        newState.turnCounter = 1
         setState(newState);
     }
+    const solver = () => {
+        let win = false;
+        const tempArray = state.box;
+        const newArray = tempArray.map(function(arrays){
+            return arrays.map(function(item){
+                if(item === 0){
+                    return (Math.random()+100);
+                }else{
+                    return item
+                }
+            })
+        });
+        let i = 0
+        newArray.map(function(col){
+            //Rows
+            if(col[0] === col[1] && col[0] === col[2]){
+                win = true;
+            }
+            //Columns
+            if(newArray[0][i] === newArray[1][i] && newArray[0][i] === newArray[2][i]){
+                win = true;
+            }
+            //Diagonals
+            if(newArray[1][1] === newArray[0+i][0] && newArray[1][1] === newArray[2-i][2] && i !== 1){
+                win = true;
+            }
+            i++;
+        })
+        
+        return win;
+    }
     return(
-        <View style={styles.grid}>
-            <View>
-                <TouchableOpacity onPress={() => ClickMe(1)} style={[{borderRightWidth: borderWidth, borderBottomWidth: borderWidth}, styles.box]}>
-                    <Text style={styles.xo}>{state.box[0]}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => ClickMe(2)} style={[{borderRightWidth: borderWidth}, styles.box]}>
-                    <Text style={styles.xo}>{state.box[1]}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => ClickMe(3)} style={[{borderRightWidth: borderWidth, borderTopWidth: borderWidth}, styles.box]}>
-                    <Text style={styles.xo}>{state.box[2]}</Text>
-                </TouchableOpacity>
+        <View style={styles.container}>
+            <Text style={styles.title}>Tic Tac Toe</Text>
+            <View style={styles.grid}>
+                {/* Column 1 */}
+                <View>
+                    <TouchableOpacity onPress={() => ClickMe(0,0)} style={[{borderRightWidth: borderWidth, borderBottomWidth: borderWidth}, styles.box]}>
+                        <Text style={styles.xo}>{state.symbols[state.box[0][0]]}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => ClickMe(1,0)} style={[{borderRightWidth: borderWidth}, styles.box]}>
+                        <Text style={styles.xo}>{state.symbols[state.box[1][0]]}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => ClickMe(2,0)} style={[{borderRightWidth: borderWidth, borderTopWidth: borderWidth}, styles.box]}>
+                        <Text style={styles.xo}>{state.symbols[state.box[2][0]]}</Text>
+                    </TouchableOpacity>
+                </View>
+                {/* Column 2 */}
+                <View>
+                    <TouchableOpacity onPress={() => ClickMe(0,1)} style={[{borderBottomWidth: borderWidth}, styles.box]}>
+                        <Text style={styles.xo}>{state.symbols[state.box[0][1]]}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => ClickMe(1,1)} style={styles.box}>
+                        <Text style={styles.xo}>{state.symbols[state.box[1][1]]}</Text> 
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => ClickMe(2,1)} style={[{borderTopWidth: borderWidth}, styles.box]}>
+                        <Text style={styles.xo}>{state.symbols[state.box[2][1]]}</Text>
+                    </TouchableOpacity>
+                </View>
+                {/* Column 3 */}
+                <View>
+                    <TouchableOpacity onPress={() => ClickMe(0,2)} style={[{borderLeftWidth: borderWidth, borderBottomWidth: borderWidth}, styles.box]}>
+                        <Text style={styles.xo}>{state.symbols[state.box[0][2]]}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => ClickMe(1,2)} style={[{borderLeftWidth: borderWidth}, styles.box]}>
+                        <Text style={styles.xo}>{state.symbols[state.box[1][2]]}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => ClickMe(2,2)} style={[{borderLeftWidth: borderWidth, borderTopWidth: borderWidth}, styles.box]}>
+                        <Text style={styles.xo}>{state.symbols[state.box[2][2]]}</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
             <View>
-                <TouchableOpacity onPress={() => ClickMe(4)} style={[{borderBottomWidth: borderWidth}, styles.box]}>
-                    <Text style={styles.xo}>{state.box[3]}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => ClickMe(5)} style={styles.box}>
-                    <Text style={styles.xo}>{state.box[4]}</Text> 
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => ClickMe(6)} style={[{borderTopWidth: borderWidth}, styles.box]}>
-                    <Text style={styles.xo}>{state.box[5]}</Text>
-                </TouchableOpacity>
-            </View>
-            <View>
-                <TouchableOpacity onPress={() => ClickMe(7)} style={[{borderLeftWidth: borderWidth, borderBottomWidth: borderWidth}, styles.box]}>
-                    <Text style={styles.xo}>{state.box[6]}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => ClickMe(8)} style={[{borderLeftWidth: borderWidth}, styles.box]}>
-                    <Text style={styles.xo}>{state.box[7]}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => ClickMe(9)} style={[{borderLeftWidth: borderWidth, borderTopWidth: borderWidth}, styles.box]}>
-                    <Text style={styles.xo}>{state.box[8]}</Text>
-                </TouchableOpacity>
+                <Text style={styles.name}>{state.name}</Text>
             </View>
             <Button title='Reset Board' onPress={resetBoard}></Button>
-                <Text>{state.status}</Text>
+            <Text>{state.status.toString()}</Text>
         </View>
     );
     
@@ -98,5 +141,15 @@ const styles = StyleSheet.create({
     xo: {
         alignSelf: 'center',
         fontSize: 40,
-    }
+    },
+    container:{
+
+    },
+    name:{
+        fontSize: 48,
+    },
+    title: {
+        fontSize: 48,
+        margin: 20,
+      },
 });
